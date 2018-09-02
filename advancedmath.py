436ed7f3942fcc83d0f7418c0fec43fe7ab52e7c
samgivian <samgivian2015@gmail.com>
2018-09-01 18:22:08 -0700

    Update advancedmath.py

--- advancedmath.py
+++ advancedmath.py
@@ -1,4 +1,5 @@
 import math
+
 def quadratic(a,b,c):
     try :
         x1=(-b+math.sqrt((b**2-4*a*c)))/(2*a)
@@ -6,6 +7,7 @@ def quadratic(a,b,c):
         print(x1,x2)
     except Exception as e:
         print(str(e))
+        
 def trig_derivatives(d):
     trig={"sin(u)":"cos(u)*u'",
           "cos(u)":"-sin(u)*u'",
@@ -15,12 +17,34 @@ def trig_derivatives(d):
           "cot(u)":"-csc(u)*csc(u)*u'"
           }
     print(str(d)+" : "+trig[d])
+    
 def average_list(d):
     s=sum(d)
     print(s/len(d))
-def circle_area(radius):
+
+def area_circle(radius):
     print(radius*radius*math.pi)
+    
 def area_triangle(base,height):
     print((base*height)/2)
+    
+def area_rectangle(a,b):
+    print(2*a*b)
+    
+def area_poly(length,sides):
+    print(length*sides)
+    
+def vol_sphere(r):
+    print(4*r**3*pi/3)
+    
 def vol_cube(length):
     print(length**3)
+    
+def vol_cylinder(radius,height):
+    print(pi*radius*radius*height)
+
+def vol_pyramid(base,height):
+    print(base*height/3)
+
+def vol_cone(radius,height):
+    print(pi*radius*radius*height/3)
